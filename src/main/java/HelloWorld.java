7b104df4802253c21efcffa6c17f082df8163cbc
James Ward <jimmywardmichael@gmail.com>
2023-04-26 13:35:58 -0500

    method challenges

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
-    public static void main(String[]args){
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int score = 0 ;
+        while (score < 5) {
+            System.out.println("i is " + score);
+            score++;
+        }
     }
+
 }
